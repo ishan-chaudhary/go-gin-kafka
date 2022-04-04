1ac552a54761f37154dcfd69098c1a413a86139a
ishan chaudhary <ishan4220@gmail.com>
2022-04-04 19:48:19 +0530

    dfd added

--- reservation-service/kafka_services/reservation.service.impl.go
+++ reservation-service/kafka_services/reservation.service.impl.go
@@ -15,7 +15,7 @@ func init() {
 	db.ConnectDB()
 }
 
-// Method to Send Product to ordered_product topic
+// Method to update Seat Matrix depending upon reservation and cancelation
 func UpdateSeatMatrix(reservationId, userId primitive.ObjectID, seat int32) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
@@ -26,7 +26,7 @@ func UpdateSeatMatrix(reservationId, userId primitive.ObjectID, seat int32) erro
 		return err
 	}
 
-	// Send product to ordered_product kafka topic
+	// Send reservations details to train service
 	reservationBody := rsv.ReserveType{
 		ID:              primitive.NewObjectID(),
 		Destination:     reservation.Destination,
